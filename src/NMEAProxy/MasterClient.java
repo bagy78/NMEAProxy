eff9dd70180e4d60a01624e317a2c9dffdf194f9
bagy78 <bagy@gmx.net>
2019-11-27 15:39:01 +0000

    Reconnect of MasterCient without visual indication

--- src/NMEAProxy/MasterClient.java
+++ src/NMEAProxy/MasterClient.java
@@ -21,39 +21,57 @@ public class MasterClient implements Runnable {
     private final int port;
     private final Starter mystarter;
     private boolean stopme;
+    private boolean tryreconnect;
     
     public MasterClient(Starter mystarter, String ip, int port) {
         this.mystarter = mystarter;
         this.stopme = false;
         this.ip = ip;
         this.port = port;
+        this.tryreconnect = true;
     }
     
      @Override
     public void run() {
-                try {
-            mysocket = new Socket(InetAddress.getByName(ip), port);
-            mysocket.setSoTimeout(5000);
-            myinputstream = mysocket.getInputStream();
-            
-        } catch (Exception e) {
-            System.out.println("Keine Verbindung" + e);
-            JOptionPane.showMessageDialog(null, "Verbidung fehlgeschlagen!");
-            stopme = true;
-        }
+        
+        connect();
+              
         while (stopme == false){
             try {
                 byte z[]= new byte[200]; 
-                
                 int laenge = myinputstream.read(z);
+                System.out.println("Laenge: " + laenge);
+                if (laenge == -1){
+                    tryreconnect = true;
+                    connect();
+                }
                 for (int i = 0; i < laenge; i++) {
                     mystarter.dowrite(z[i]);
                 }
             } catch (Exception e) {
+                System.out.println("neue Meldung xyz: " + e);
             }
         }
     }
     
+    
+     private void connect(){    
+        while (tryreconnect == true && stopme == false){
+            try {
+              mysocket = new Socket(InetAddress.getByName(ip), port);
+              mysocket.setSoTimeout(5000);
+              myinputstream = mysocket.getInputStream();
+              tryreconnect = false;
+              
+            
+           } catch (Exception e) {
+             System.out.println("Keine Verbindung" + e);
+              //JOptionPane.showMessageDialog(null, "Verbidung fehlgeschlagen!");
+              tryreconnect = true;
+              //stopme = true;
+          }
+        }
+    }
     
     public void setStopme(boolean stopme) {
         
